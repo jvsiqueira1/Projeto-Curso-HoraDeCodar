49093f1d38674f47a8a7f898db17047f476e29a2
João Vitor de Siqueira Campos <vitor.siqueira@centralit.com.br>
2024-02-09 15:29:02 -0400

    Revisar o Código de criação dos Projetos no DB

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Container from "./components/layout/Container";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Projects from "./components/pages/Projects";
+import Project from "./components/pages/Project";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
           <Route path="/contact" element={<Contact />} />
           <Route path="/newproject" element={<NewProject />} />
           <Route path="/projects" element={<Projects />} />
+          <Route path="/projects/:id" element={<Project />} />
         </Routes>
       </Container>
       <Footer />
--- src/components/form/Select.js
+++ src/components/form/Select.js
@@ -4,8 +4,18 @@ function Select({ text, name, options, handleOnChange, value }) {
   return (
     <div className={styles.form_control}>
       <label htmlFor={name}>{text}:</label>
-      <select name={name} id={name}>
-        <option>Seleciona uma opção</option>
+      <select
+        name={name}
+        id={name}
+        onChange={handleOnChange}
+        value={value || ""}
+      >
+        <option>Selecione uma opção</option>
+        {options.map((options) => (
+          <option value={options.id} key={options.id}>
+            {options.name}
+          </option>
+        ))}
       </select>
     </div>
   );
